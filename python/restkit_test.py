4d5bde2b95934c31cf9d1d4aaba059791eedda4d
Padraig Gleeson <p.gleeson@gmail.com>
2012-06-13 14:22:07 +0100

    Test for using restkit

---
+++ python/restkit_test.py
@@ -0,0 +1,21 @@
+'''
+Preliminary tests using restkit...
+'''
+
+from restkit import Resource
+res = Resource('http://www.opensourcebrain.org:8080')
+r = res.get('/users/4.json')
+
+import json
+
+jr = json.loads(r.body_string())
+
+print "User: "+str(jr["user"]["id"]) +" is "+jr["user"]["firstname"] +" "+jr["user"]["lastname"] 
+
+p = res.get('/projects.json')
+
+
+jp = json.loads(p.body_string())
+
+for project in jp["projects"]:
+    print "Project: "+ project["name"]+" was last updated on "+ project["updated_on"]
