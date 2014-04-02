cab79694ddf0c0e1911ac924f4a8b8f5a42e3ffb
Padraig Gleeson <p.gleeson@gmail.com>
2014-04-02 13:53:55 +0100

    Update CheckModelDB

--- python/CheckModelDB.py
+++ python/CheckModelDB.py
@@ -3,15 +3,15 @@ Script to check ModelDB references
 '''
 
 
-from __init__ import get_project_list, get_custom_field, print_custom_field
+import osb
 
 projects = 0
 with_modeldb = 0
     
-for project in get_project_list():
+for project in osb.get_project_list():
 
-    isProj = get_custom_field(project, 'Category') == 'Project'
-    mdb = get_custom_field(project, 'ModelDB reference')
+    isProj = osb.get_custom_field(project, 'Category') == 'Project'
+    mdb = osb.get_custom_field(project, 'ModelDB reference')
     hasMDB = mdb != None and len(mdb)>0
     
     
@@ -22,7 +22,7 @@ for project in get_project_list():
         if hasMDB:
             print "    OSB link:%shttp://opensourcebrain.org/projects/%s" % \
                                                 (" "*22,project["identifier"])
-            print_custom_field(project, 'ModelDB reference')
+            osb.print_custom_field(project, 'ModelDB reference')
             with_modeldb +=1
         else:
             print "    No ModelDB info for model"
