fbe27c193f7ca8d6e1e7725efa206dd64f62139d
Padraig Gleeson <p.gleeson@gmail.com>
2015-03-11 10:17:20 +0000

    More checks to ensure github url set in OSB is correctly formatted
    Some correcting of formatting

--- python/osb/Project.py
+++ python/osb/Project.py
@@ -4,7 +4,7 @@ from __init__ import *
 from Repository import *
 
 class Project(OSBEntity):
-    
+
     attrs = {
         'IDENTIFIER': 'identifier',
         'NAME': 'name',
@@ -14,30 +14,29 @@ class Project(OSBEntity):
         'UPDATED_ON': 'updated_on',
 
         'CATEGORY': 'Category',
-    
+
         'TAGS': 'Tags',
 
         'MODELDB_REFERENCE': 'ModelDB reference',
-        
+
         'GITHUB_REPO_ATTR': "GitHub repository",
         'GITHUB_REPO_STR': "GitHub repository",
 
         'GITHUB_REPO': "github_repo", # Used for GitHubRepository object
 
-    
         'STATUS': 'Status info',
-        
+
         'ENDORSEMENT': 'Endorsement',
-        
+
         'SPECIES': 'Specie',
         'SPECIE': 'specie',
-        
+
         'BRAIN_REGION': "Brain region",
-        
+
         'CELL_TYPE': "Cell type",
-        
+
         'SPINE_CLASSIFICATION': 'Spine classification',
-        
+
         'NEUROLEX_IDS_CELLS': 'NeuroLex Ids: Cells',
         }
 
@@ -52,7 +51,7 @@ class Project(OSBEntity):
 
     def __init__(self, info_array):
         OSBEntity.__init__(self, info_array)
-    
+
     def __getattr__(self, name):
         #print("Checking for attr %s..."%(name))
         #print self.info_array.keys()
@@ -72,9 +71,9 @@ class Project(OSBEntity):
 
         if name == 'TAGS':
             value = str(value).split(",") if value is not None else []
-        
+
         elif name == 'GITHUB_REPO':
-	    repo_str = self.get_custom_field('GitHub repository')
+            repo_str = self.get_custom_field('GitHub repository')
             #print repo_str
             value = GitHubRepository.create(repo_str)
 
@@ -89,28 +88,28 @@ class Project(OSBEntity):
 
         try:
             v = float(value)
-	    if v == int(v):
-		return int(v)
-	    else:
-		return v
+            if v == int(v):
+                return int(v)
+            else:
+                return v
         except ValueError:
             return value
         except TypeError:
             return value
-        
+
     def __getitem__(self, name):
         #print("Checking for item %s..."%(name))
         return None
-        
+
     def __str__(self):
         return "OSB Project: %s (%s)"%(self.name, self.identifier)
-        
+
     def is_standard_project(self):
         return self.category == self.CATEGORY_PROJECT
-    
+
     def is_showcase(self):
         return self.category == self.CATEGORY_SHOWCASE
-    
+
     @classmethod
     def get_data(cls, project_identifier, fuzzy=False):
         result = None
@@ -142,17 +141,17 @@ class Project(OSBEntity):
     def get(cls, project_identifier, fuzzy=False):
         project_data = cls.get_data(project_identifier, fuzzy=fuzzy)
         return cls(project_data)
-    
-        
+
+
 if __name__ == "__main__":
-   
+
     project = Project.get('thalamocortical')
-    
+
     print("Project %s, %s: %s"%(project.id, project.identifier, project.name))
     print("Category: %s (Standard project? %s)"%(project.category, project.is_standard_project()))
     print("ModelDB reference: %s"%(project.modeldb_reference))
     print("Endorsement: %i"%(project.endorsement==1))
     print("GitHub repo str: %s"%(project.github_repo_str))
     print("GitHub repo: %s"%(project.github_repo))
-    
+
     print("Done")
