e0cc3121a9c48385513fe876dcba0929f78f832e
Robert A McDougal <robert.mcdougal@yale.edu>
2017-12-29 15:17:10 -0500

    updated CheckModelDB.py to correctly handle missing ModelDB links

--- python/examples/CheckModelDB.py
+++ python/examples/CheckModelDB.py
@@ -15,7 +15,7 @@ for project in osb.get_projects(min_curation_level="None"):
         print("\n--------   Project: %s\n" % project.name)
         projects+=1
 
-        if project.modeldb_reference:
+        if project.modeldb_reference and project.modeldb_reference != 'None':
             print("    OSB link:%shttp://opensourcebrain.org/projects/%s" % (" "*22, project.identifier))
             print("    ModelDB link:%shttp://senselab.med.yale.edu/ModelDB/ShowModel.asp?model=%s" % (' '*18, project.modeldb_reference))
             with_modeldb +=1
